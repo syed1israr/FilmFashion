da6804088270df40698b6724a4bffda35ccfc30f
syedFaraz <syedfaraz173@gmail.com>
2024-02-20 08:26:29 +0530

    fixed sidenav

--- src/Components/Templates/HorizantalCards.jsx
+++ src/Components/Templates/HorizantalCards.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const  HorizantalCards=({data})=> {
 
   return (
-        <div className='w-[100%] h-[40vh]  flex overflow-y-hidden mb-3 '  >
+        <div className='w-[100%]  h-[40vh]  flex overflow-y-hidden mb-3 '  >
             {
-                data.map((data,i)=><div key={i} className=' ml-[20px] min-w-[20%] w-[50%]  bg-zinc-900 mr-5 rounded-xl shadow-slate-900 shadow-xl'>
-                    <img className='w-full h-[45%] object-cover rounded-xl'
+                data.map((data,i)=><div key={i} className=' ml-[20px] min-w-[20%] w-[50%]  bg-black mr-5 rounded shadow-slate-900 shadow-xl'>
+                    <img className='w-full h-[45%] object-cover rounded-[5px]'
                     src={`https://image.tmdb.org/t/p/original/${ data.backdrop_path || data.profile_path || data.poster_path }  `}alt="" />
                      <div className='text-white h-[65%]'>
                      <h1 
--- src/Components/Templates/SideNav.jsx
+++ src/Components/Templates/SideNav.jsx
@@ -6,12 +6,12 @@ const SideNav = () => {
 
 
   return (
-    <div className="w-[20%] h-full  border-r-2 border-zinc-200 p-10 ">
+    <div className="w-[18%] h-full  border-r-2 border-zinc-200 p-10 ">
       <h1 className='text-2xl text-white font-bold'>
       <i className=" text-[#6556CD] text-2xl ri-arrow-up-circle-fill mr-3"></i>
       <span className='text-2xl'>Databse</span>
       </h1>
-      <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
+      <nav className='flex flex-col text-zinc-400 text-xl gap-3 mb-3'>
       <h1 className='text-white font-semibold text-xl mt-10 mb-5 '>News Feeds</h1>
       <Link className='hover:bg-[#6556CD] p-5 text-white rounded-lg duration-300'> <i className="ri-firefox-fill mr-2"></i> trending</Link>
       <Link className='hover:bg-[#6556CD] p-5 text-white rounded-lg duration-300' ><i className="ri-magic-fill mr-2"></i> Popular </Link>
@@ -21,8 +21,8 @@ const SideNav = () => {
       </nav>
       <hr  className=' border-none h-[1px] bg-zinc-400'/>
       <nav className='flex flex-col text-zinc-400 text-xl gap-3'>
-      <h1 className='text-white font-semibold text-xl mt-10 mb-5 '>Website information</h1>
-      <Link className='hover:bg-[#6556CD] p-5 text-white rounded-lg duration-300'><i className="ri-customer-service-2-line mr-2"></i>contact Us</Link>
+      <h1 className='text-white font-semibold text-xl mt-3 mb-2 '>Website information</h1>
+      <Link className='hover:bg-[#6556CD] p-5 text-white rounded-lg duration-300 mt-2'><i className="ri-customer-service-2-line mr-2"></i>contact Us</Link>
       <Link className='hover:bg-[#6556CD] p-5 text-white rounded-lg duration-300  -mt-6'> <i className="ri-goblet-line mr-2"></i> About</Link>
       </nav>
     </div>
